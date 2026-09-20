020110bbb7e242fcd1fb198b9a0c2c69e9e68a21
iwonz <iwonz@icloud.com>
2026-09-20 08:39:44 +0300

    fix: verify inferred Homebrew version

--- Formula/courier.rb
+++ Formula/courier.rb
@@ -2,6 +2,7 @@ class Courier < Formula
   desc "Safely transfer files and directories between local and SSH endpoints"
   homepage "https://github.com/iwonz/courier"
   url "https://github.com/iwonz/courier/releases/download/v0.2.0/courier_0.2.0_source.tar.gz"
+  # Homebrew infers version "0.2.0" from the source archive URL.
   sha256 "7b2afa2e88778d352927d9fe38966fb3a5a267e17c56beafb468608748d65204"
   license "MIT"
 
